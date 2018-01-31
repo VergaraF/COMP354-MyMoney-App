5ba241f9c8455d78c2a95078ee0e45dfe0547925
Giles Kisife <giles_kisife@yahoo.ca>
2018-01-30 21:36:12 -0500

    Update MainPanel.java

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -1,4 +1,5 @@
 
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -7,6 +8,7 @@ import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,121 +24,191 @@ import javax.swing.JTextField;
  */
 public class MainPanel extends JFrame {
 
-    private final JTextField userArea;
-    private final JPasswordField passwordArea;
-    private final JButton logBtn;
-    private final JButton cancelBtn;
-    private final JLabel user;
-    private final JLabel pass;
-    private final JMenuBar menuBar;
-    private JMenu menu;
-    private JMenuItem menuItem;
-    private BufferedImage appIcon;
+	private final JTextField userArea;
+	private final JPasswordField passwordArea;
+	private final JButton logBtn;
+	private final JButton cancelBtn;
+	private final JLabel user;
+	private final JLabel pass;
+	private JLabel message;
+	private final JMenuBar menuBar;
+	private JMenu menu;
+	private JMenuItem menuItem;
+	private BufferedImage appIcon;
+	private static int loginCount = -1;
 
-    public MainPanel() {
-        super("MyMoney");
+	public MainPanel() {
+		super("MyMoney");
 
-        //Menu bar settings
-        menuBar = new JMenuBar();
-        placeMenu(menuBar, menu, menuItem);
+		// Menu bar settings
+		menuBar = new JMenuBar();
+		placeMenu(menuBar, menu, menuItem);
 
-        //Buttons, text fields and icon settings
-        appIcon = chooseIconImage(appIcon, "money.png");
+		// Buttons, text fields and icon settings
+		appIcon = chooseIconImage(appIcon, "money.png");
 
-        logBtn = new JButton("Log in");
-        logInOnClick();
+		logBtn = new JButton("Log in");
+		logInOnClick();
 
-        cancelBtn = new JButton("Cancel");
-        closeOnCancelClick(cancelBtn);
+		cancelBtn = new JButton("Cancel");
+		closeOnCancelClick(cancelBtn);
 
-        user = new JLabel("Username");
-        userArea = new JTextField("", 15);
+		user = new JLabel("Username");
+		userArea = new JTextField("", 15);
 
-        pass = new JLabel("Password");
-        passwordArea = new JPasswordField("", 15);
-    }
+		pass = new JLabel("Password");
+		passwordArea = new JPasswordField("", 15);
 
-    public void setupForPanel() {
-        //Layout settings
-        setLayout(new GridBagLayout());
-        placeButtons();
+		message = new JLabel("Login or Create an account");
 
-        //Panel settings
-        setSize(300, 200);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setIconImage(appIcon);
-        setJMenuBar(menuBar);
-        setResizable(false);
-        setVisible(true);
-    }
+	}
 
-    private void placeButtons() {
-        GridBagConstraints gb = new GridBagConstraints();
+	public void setupForPanel() {
+		// Layout settings
+		setLayout(new GridBagLayout());
+		placeButtons();
 
-        gb.gridx = 0;
-        gb.gridy = 0;
-        add(user, gb);
+		// Panel settings
+		setSize(800, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setIconImage(appIcon);
+		setJMenuBar(menuBar);
+		setVisible(true);
+	}
 
-        gb.gridx = 1;
-        gb.gridy = 0;
-        add(userArea, gb);
+	private void placeButtons() {
+		GridBagConstraints gb = new GridBagConstraints();
 
-        gb.gridx = 0;
-        gb.gridy = 1;
-        add(pass, gb);
+		gb.gridx = 0;
+		gb.gridy = 0;
+		add(user, gb);
 
-        gb.gridx = 1;
-        gb.gridy = 1;
-        add(passwordArea, gb);
+		gb.gridx = 1;
+		gb.gridy = 0;
+		add(userArea, gb);
 
-        gb.insets = new Insets(10, 0, 0, 0);
-        gb.gridx = 0;
-        gb.gridy = 2;
-        add(logBtn, gb);
+		gb.gridx = 0;
+		gb.gridy = 1;
+		add(pass, gb);
 
-        gb.insets = new Insets(10, 0, 0, -95);
-        gb.gridx = 1;
-        gb.gridy = 2;
-        add(cancelBtn, gb);
-    }
+		gb.gridx = 1;
+		gb.gridy = 1;
+		add(passwordArea, gb);
 
-    private void placeMenu(JMenuBar menuBar, JMenu menu, JMenuItem menuItem) {
-        menu = new JMenu("Registration");
-        menuBar.add(menu);
-        menuItem = new JMenuItem("Account registration", KeyEvent.VK_A);
-        menu.add(menuItem);
+		gb.insets = new Insets(10, 0, 0, 0);
+		gb.gridx = 0;
+		gb.gridy = 2;
+		add(logBtn, gb);
 
-        menu = new JMenu("Help");
-        menuBar.add(menu);
-        menuItem = new JMenuItem("Documentation", KeyEvent.VK_D);
-        menu.add(menuItem);
+		gb.insets = new Insets(10, 0, 0, -95);
+		gb.gridx = 1;
+		gb.gridy = 2;
+		add(cancelBtn, gb);
 
-        menu = new JMenu("About");
-        menuBar.add(menu);
-        menuItem = new JMenuItem("MyMoney", KeyEvent.VK_M);
-        menu.add(menuItem);
-    }
+		gb.gridx = 0;
+		gb.gridy = 3;
+		add(message, gb);
+	}
 
-    public final void closeOnCancelClick(JButton btn) {
-        btn.addActionListener((ActionEvent e) -> {
-            dispose();
-        });
-    }
+	private void placeMenu(JMenuBar menuBar, JMenu menu, JMenuItem menuItem) {
+		menu = new JMenu("Registration");
+		menuBar.add(menu);
+		// menuItem = new JMenuItem("Account Registration", KeyEvent.VK_A);
 
-    public final BufferedImage chooseIconImage(BufferedImage icon, String path) {
-        try {
-            icon = ImageIO.read(getClass().getClassLoader().getResource(path));
-        } catch (IOException e) {
-            e.getMessage();
-        }
-        return icon;
-    }
+		menuItem = new JMenuItem(new AbstractAction("Account Registration") {
 
-    private void logInOnClick() {
-        logBtn.addActionListener((ActionEvent e) -> {
-            MainPanel panel = new FinancePanel();
-            panel.setupForPanel();
-            dispose();
-        });
-    }
+			public void actionPerformed(ActionEvent e) {
+
+				// Code for registration menuAction
+				RegistrationPanel regPanel = new RegistrationPanel();
+				regPanel.setupForPanel();
+
+			}
+		});
+
+		menu.add(menuItem);
+
+		menu = new JMenu("Help");
+		menuBar.add(menu);
+		menuItem = new JMenuItem("Documentation", KeyEvent.VK_D);
+		menu.add(menuItem);
+
+		menu = new JMenu("About");
+		menuBar.add(menu);
+		menuItem = new JMenuItem("MyMoney", KeyEvent.VK_M);
+		menu.add(menuItem);
+	}
+
+	public final void closeOnCancelClick(JButton btn) {
+		btn.addActionListener((ActionEvent e) -> {
+			dispose();
+		});
+	}
+
+	public final BufferedImage chooseIconImage(BufferedImage icon, String path) {
+		try {
+			icon = ImageIO.read(getClass().getClassLoader().getResource(path));
+		} catch (IOException e) {
+			e.getMessage();
+		}
+		return icon;
+	}
+
+	private void logInOnClick() {
+
+		logBtn.addActionListener((ActionEvent e) -> {
+			MainPanel panel = new BankPanel();
+			// this.dispose();
+
+			if (LogIn.validateUser(sendUserName(), sendUserPassword())) {
+
+				panel.setupForPanel();
+				userArea.setText("");
+				passwordArea.setText("");
+				loginCount =-1;
+
+			}
+			else{
+				//Maximum number of login attempts is 3
+				if (loginCount < 4) {
+					setMessage("Username and password did not match! Try again");
+
+				} else {
+
+					setMessage("Too many login attempts! Exiting!!");
+
+					try {
+                        //Wait a 5 seconds to display a message before closing the panel.
+						Thread.sleep(5000);
+
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+					this.dispose();
+
+				}
+			}
+
+		});
+		// Count the number of login attempts.
+		loginCount++;
+	}
+
+	public String sendUserName() {
+
+		return userArea.getText();
+	}
+
+	public String sendUserPassword() {
+
+		return passwordArea.getText();
+	}
+
+	// Method sets a directive message on the message board panel
+	public void setMessage(String text) {
+		message.setText(text);
+	}
+
 }
