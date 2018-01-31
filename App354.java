6e3def221339ea0467eef8fec5c425b91de8ec73
Giles Kisife <giles_kisife@yahoo.ca>
2018-01-30 21:38:28 -0500

    Add files via upload

--- App354.java
+++ App354.java
@@ -11,11 +11,18 @@ public class App354 {
     /**
      * @param args the command line arguments
      */
+	
+	
     public static void main(String[] args) {
+    	
+    	MyMoneyController.controller();
+    	
+    	/**
         SwingUtilities.invokeLater(() -> {
             MainPanel p = new MainPanel();
             p.setupForPanel();
         });
+        */
     }
 
 }
