bccf4638173047989a6ac9b1cc03492ed927f718
Jean-LoupJohnston <35584056+Jean-LoupJohnston@users.noreply.github.com>
2018-01-31 22:13:41 -0500

    Update Model.java

--- Model.java
+++ Model.java
@@ -49,6 +49,43 @@ public class Model {
 	public void setInputFileName(String inputFileName) {
 		this.inputFileName = inputFileName;
 	}
+	public static String displayTransactions() {
+		Scanner s1 = null;
+
+		try {
+			s1 = new Scanner(new FileInputStream("datafiles/Transactions"));
+		} catch (FileNotFoundException e) {
+			System.out.print("file could not be found.");
+
+		}
+		String transactions = "<html>";
+
+		while (s1.hasNextLine()) {
+			transactions += s1.nextLine() + "<br>";
+		}
+
+		s1.close();
+
+		return transactions;
+	}
+
+	public static String displaySavings() {
+		Scanner s1 = null;
+
+		try {
+			s1 = new Scanner(new FileInputStream("datafiles/Bank"));
+		} catch (FileNotFoundException e) {
+			System.out.print("File could not be found.");
+
+		}
+		String savings = "";
+
+		savings = s1.next();
+
+		s1.close();
+
+		return savings;
+	}
 
 	/**
 	 * Use case one: As a user, user is able to create and access his account
