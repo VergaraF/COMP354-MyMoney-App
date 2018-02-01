a64454d1485bb0b4a6851bad5643e262401d1028
Jean-LoupJohnston <35584056+Jean-LoupJohnston@users.noreply.github.com>
2018-01-31 19:34:03 -0500

    Update BankPanel.java

--- BankPanel.java
+++ BankPanel.java
@@ -1,4 +1,4 @@
-
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -14,68 +14,99 @@ import javax.swing.JTextField;
  */
 public class BankPanel extends MainPanel {
 	private JButton clsButton;
-	private BufferedImage appIcon;
-	private final JLabel bankInfo;
-	private final JLabel Savings;
-	private final JLabel SavingsInfo;
-	private final JLabel Transactions;
-	private final JLabel TransactionsInfo;
+    private BufferedImage appIcon;
+    private final JLabel bankInfo;
+    private final JLabel Savings;
+    private final JLabel SavingsInfo;
+    private final JLabel Transactions;
+    private final JLabel TransactionsInfo;
+    
+    User u = new User();
 
-	User u = new User();
+    public BankPanel() {
+        appIcon = chooseIconImage(appIcon, "money.png");
+        bankInfo = new JLabel("Banking Information");
+        Savings = new JLabel("<HTML><U>Savings</U></HTML>");
+        SavingsInfo = new JLabel(u.displaySavings());
+        Transactions = new JLabel("<HTML><U>Transactions</U></HTML>");
+        TransactionsInfo = new JLabel(u.displayTransactions());
+        clsButton = new JButton("Exit");
+        closeOnCancelClick(clsButton);
+    }
+    
+    
 
-	public BankPanel() {
-		appIcon = chooseIconImage(appIcon, "money.png");
-		bankInfo = new JLabel("Banking Information");
-		Savings = new JLabel("Savings");
-		SavingsInfo = new JLabel(u.displaySavings());
-		Transactions = new JLabel("Transacations");
-		TransactionsInfo = new JLabel(u.displayTransactions());
-		clsButton = new JButton("Exit");
-		closeOnCancelClick(clsButton);
-	}
+    @Override
+    public void setupForPanel() {
+    	
+        setSize(600, 500);
+        setResizable(false);
+        setVisible(true);
+        setLayout(new GridBagLayout());
+    	placeButtons();
+        
+        setIconImage(appIcon);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        bankInfo.setFont(new Font("SansSerif", Font.PLAIN, 28));
+        Transactions.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        Savings.setFont(new Font("SansSerif", Font.PLAIN, 20));
+    }
+    
+    private void placeButtons()
+    {
+    	GridBagConstraints gb = new GridBagConstraints();
+    	
+    	
+    	gb.insets = new Insets(5, 5, 5, 5);
+        gb.gridx = 2;
+        gb.gridwidth = 3;
+        gb.gridy = 0;
+        gb.weightx = 1;
+        gb.weighty = 1;
+        
+        add(bankInfo, gb);
+        
+        
+        
+       
+        
+        gb.insets = new Insets(5, 5, 5, 5);
+        gb.gridx = 1;
+        gb.gridwidth = 2;
+        gb.gridy = 1;
+        add(Savings, gb);
+        
+        gb.insets = new Insets(5, 5, 5, 5);
+        gb.gridx = 3;
+        gb.gridwidth = 2;
+        gb.gridy = 1;
+        add(Transactions, gb);
+        
+        gb.weighty = 10;
+        
+        gb.anchor = GridBagConstraints.NORTH;
+        
+        gb.insets = new Insets(5, 5, 5, 5);
+        gb.gridx = 1;
+        gb.gridwidth = 2;
+        gb.gridy = 2;
+        add(SavingsInfo, gb);
+        
+        
+        gb.insets = new Insets(5, 5, 5, 5);
+        gb.gridx = 3;
+        gb.gridwidth = 2;
+        gb.gridy = 2;
+        gb.gridheight= 2;
+        add(TransactionsInfo, gb);
+        
+        gb.anchor = GridBagConstraints.NORTH;
+        gb.weighty = 3; 
+        gb.insets = new Insets(5, 5, 5, 5);
+        gb.gridx = 2;
+        gb.gridwidth = 3;
+        gb.gridy = 3;
+        add(clsButton, gb);
+    }
 
-	@Override
-	public void setupForPanel() {
-		setLayout(new GridBagLayout());
-		placeButtons();
-		setSize(600, 500);
-		setResizable(true);
-		setVisible(true);
-		setIconImage(appIcon);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-
-	private void placeButtons() {
-		GridBagConstraints gb = new GridBagConstraints();
-
-		gb.insets = new Insets(300, 0, 0, 0);
-		gb.gridx = 1;
-		gb.gridy = 2;
-		add(clsButton, gb);
-
-		gb.insets = new Insets(-400, 0, 0, 0);
-		gb.gridx = 1;
-		gb.gridy = 2;
-		add(bankInfo, gb);
-
-		gb.insets = new Insets(-300, 0, 0, 300);
-		gb.gridx = 1;
-		gb.gridy = 2;
-		add(Savings, gb);
-
-		gb.insets = new Insets(-250, 0, 0, 300);
-		gb.gridx = 1;
-		gb.gridy = 2;
-		add(SavingsInfo, gb);
-
-		gb.insets = new Insets(-300, 0, 0, -200);
-		gb.gridx = 1;
-		gb.gridy = 2;
-		add(Transactions, gb);
-
-		gb.insets = new Insets(-230, 0, 0, -200);
-		gb.gridx = 1;
-		gb.gridy = 2;
-		add(TransactionsInfo, gb);
-	}
 }
