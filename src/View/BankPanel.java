ea92414890dd48b7b64bf966857ac967004ca200
michaelmxu <32373152+michaelmxu@users.noreply.github.com>
2018-03-17 20:47:13 -0400

    Add files via upload

--- src/View/BankPanel.java
+++ src/View/BankPanel.java
@@ -14,6 +14,12 @@ import javax.swing.JLabel;
 
 import Controller.*;
 import Model.Model;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
+import javax.swing.JScrollBar;
 
 /**
  *
@@ -30,11 +36,20 @@ public class BankPanel extends MainPanel {
 	private final JLabel Savings;
 	private final JLabel SavingsInfo;
 	private final JLabel Transactions;
+	//michael
+	private final JLabel Advices;
+	private final JTextPane AdviceInfo;
+	//
 	private final JLabel TransactionsInfo;
 	
 	TransactionsController transactionController = new TransactionsController();
 
 	public BankPanel() {
+		getContentPane().setLayout(null);
+		
+		
+		
+		
 		//[Fabian]
 		System.out.println("Generating data files");
 		
@@ -49,6 +64,24 @@ public class BankPanel extends MainPanel {
 		SavingsInfo = new JLabel(Model.displaySavings());
 		Transactions = new JLabel("<HTML><B>Transactions</B></HTML>");
 		TransactionsInfo = new JLabel(transactionController.getRawStringTransacations());
+		
+		//michael
+		Advice finan_Advice = new Advice();
+		finan_Advice.execute();
+		Advices = new JLabel("<HTML><B>Advice</U></HTML>");
+		//AdviceInfo = new JLabel(finan_Advice.Advice);
+		
+		AdviceInfo = new JTextPane();
+		AdviceInfo.setText(finan_Advice.Advice);
+		//AdviceInfo.setBounds(300, 50, 10, 40);
+		getContentPane().add(AdviceInfo);
+		
+		/*
+		JTextPane txtpnHello = new JTextPane();
+		txtpnHello.setText("hello");
+		txtpnHello.setBounds(287, 199, 6, 20);
+		getContentPane().add(txtpnHello);*/
+		//
 		
 		clsButton = new JButton("Exit");
 		budgetButton = new JButton("Budget");
@@ -83,10 +116,10 @@ public class BankPanel extends MainPanel {
 	@Override
 	public void setupForPanel() {
 
-		setSize(900, 900);
+		setSize(2000, 2000);
 		setResizable(false);
 		setVisible(true);
-		setLayout(new GridBagLayout());
+		getContentPane().setLayout(new GridBagLayout());
 		placeButtons();
 
 		setIconImage(appIcon);
@@ -94,6 +127,9 @@ public class BankPanel extends MainPanel {
 		bankInfo.setFont(new Font("SansSerif", Font.PLAIN, 28));
 		Transactions.setFont(new Font("SansSerif", Font.PLAIN, 20));
 		Savings.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		//michael
+		Advices.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		//
 	}
 
 	private void placeButtons() {
@@ -105,51 +141,68 @@ public class BankPanel extends MainPanel {
 		gb.gridy = 0;
 		gb.weightx = 1;
 		gb.weighty = 1;
-
-		add(bankInfo, gb);
+		getContentPane().add(bankInfo, gb);
 
 		gb.insets = new Insets(5, 5, 5, 5);
 		gb.gridx = 1;
 		gb.gridwidth = 2;
 		gb.gridy = 1;
-		add(Savings, gb);
-
+		getContentPane().add(Advices, gb);
+		
+		//michael
 		gb.insets = new Insets(5, 5, 5, 5);
 		gb.gridx = 3;
 		gb.gridwidth = 2;
 		gb.gridy = 1;
-		add(Transactions, gb);
+		getContentPane().add(Savings, gb);
+		//
+		
+		gb.insets = new Insets(5, 5, 5, 5);
+		gb.gridx = 5;
+		gb.gridwidth = 2;
+		gb.gridy = 1;
+		getContentPane().add(Transactions, gb);
 
 		gb.weighty = 10;
 
 		gb.anchor = GridBagConstraints.NORTH;
 
 		gb.insets = new Insets(5, 5, 5, 5);
-		gb.gridx = 1;
+		gb.gridx = 3;
 		gb.gridwidth = 2;
 		gb.gridy = 2;
-		add(SavingsInfo, gb);
+		getContentPane().add(SavingsInfo, gb);
 
 		gb.insets = new Insets(55, 5, 5, 5);
-		gb.gridx = 3;
+		gb.gridx = 5;
 		gb.gridwidth = 4;
 		gb.gridy = 2;
 		gb.gridheight = 2;
-		add(TransactionsInfo, gb);
+		getContentPane().add(TransactionsInfo, gb);
+		
+		
+		gb.anchor = GridBagConstraints.NORTH;
+		gb.insets = new Insets(55, 5, 5, 5);
+		gb.gridx = 2;
+		gb.gridwidth = 2;
+		gb.gridy = 2;
+		gb.gridheight = 2;
+		getContentPane().add(AdviceInfo, gb);
+		
 		
 		gb.insets = new Insets(5, 1, 5, 5);
-		gb.gridx = 3;
+		gb.gridx = 5;
 		gb.gridwidth = 2;
 		gb.gridy = 2;
 		gb.gridheight = 2;
-		add(rawButton, gb);
+		getContentPane().add(rawButton, gb);
 		
 		gb.insets = new Insets(5, 150, 5, 5);
-		gb.gridx = 3;
+		gb.gridx = 5;
 		gb.gridwidth = 2;
 		gb.gridy = 2;
 		gb.gridheight = 2;
-		add(groupByDateButton, gb);
+		getContentPane().add(groupByDateButton, gb);
 
 		gb.anchor = GridBagConstraints.NORTH;
 		gb.weighty = 3;
@@ -157,17 +210,17 @@ public class BankPanel extends MainPanel {
 		gb.gridx = 1;
 		gb.gridwidth = 3;
 		gb.gridy = 3;
-		add(clsButton, gb);
+		getContentPane().add(clsButton, gb);
 
 		gb.anchor = GridBagConstraints.NORTH;
 		gb.weighty = 3;
 		gb.insets = new Insets(5, 5, 5, 5);
-		gb.gridx = 3;
+		gb.gridx = 5;
 		gb.gridwidth = 3;
 		gb.gridy = 3;
-		add(budgetButton, gb);
-
+		getContentPane().add(budgetButton, gb);
+		
+		
 		setResizable(true);
 	}
-
 }
